fb1aed0f92687f73fdd236b7ebb10f54ed668169
Jesus <jesusdavidpoloespitia@gmail.com>
2023-03-27 20:13:02 -0500

    Dominio creado
    Se crea el Dominio a manejar en en el proyecto

---
+++ Dominio.CasosdeUso/Gateway/IElectrodomesticoCasodeUso.cs
@@ -0,0 +1,23 @@
+﻿using Dominio.Entitadas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.CasosdeUso.Gateway
+{
+    public interface IElectrodomesticoCasodeUso
+    {
+        Task<Electrodomesticos> AgregarElectrodomestico(Electrodomesticos electrodomesticos);
+
+        Task<List<Electrodomesticos>> ObtenerListaElectrodomesticos();
+
+        Task<Electrodomesticos> ObtenerElectrodomesticoPorId(int id);
+        
+        Task<Electrodomesticos> EliminarElectrodomesticoPorId(int id);
+
+        Task<Electrodomesticos> EditarElectrodomestico(Electrodomesticos electrodomesticos);
+    }
+}
---
+++ Dominio.Entitadas/Comandos/InsertarNuevoElectodomestico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entitadas.Comandos
+{
+    public class InsertarNuevoElectodomestico
+    {
+        public string Nombre_Electrodomestico { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public DateTime Fecha_Creacion { get; set; }
+        public DateTime Fecha_Ingreso { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
